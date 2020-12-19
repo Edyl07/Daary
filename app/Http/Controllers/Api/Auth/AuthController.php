60e0a2368e6cb46050573056ce09de147b448ffb
Edyl07 <demba94diagana@gmail.com>
2020-12-19 21:57:55 +0000

    update AuthController

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -12,6 +12,7 @@ use Tymon\JWTAuth\Exceptions\JWTException as ExceptionsJWTException;
 use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
